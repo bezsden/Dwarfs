a23dcf12b790b569f4257b0a9cc5b638ab2d3c60
bezsden <bess.lex@gmail.com>
2019-03-26 17:06:05 +0200

    List, compare/sort Dwrafs using their names

---
+++ src/org/plane/Dwarf.java
@@ -0,0 +1,37 @@
+package org.plane;
+
+public class Dwarf implements Comparable<Dwarf> {
+    private String name;
+    private Integer age;
+
+    public void say() {
+        System.out.println("I am "+name);
+    }
+
+
+    public int compareTo(Dwarf o) {
+        return  this.getName().compareTo(o.getName());
+    }
+
+    public Dwarf(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
---
+++ src/org/plane/Sort.java
@@ -0,0 +1,40 @@
+package org.plane;
+
+import javafx.collections.transformation.SortedList;
+
+import java.util.*;
+
+
+public class Sort {
+    public static void main(String[] args) {
+        Dwarf dwf1 = new Dwarf("Tory",200);
+        Dwarf dwf2 = new Dwarf("Rory",180);
+        Dwarf dwf3 = new Dwarf("Bary",150);
+
+        //dwf1.say();
+        //dwf2.say();
+        //dwf3.say();
+        //System.out.println( dwf1.compareTo(dwf2));
+
+        // sort Dwarfs by something.
+        // First is Name, Age, Name and age, age and name
+        // First use comparable, then comparator
+
+        List<Dwarf> dwl = new ArrayList<Dwarf>();
+        dwl.add(dwf1);
+        dwl.add(dwf2);
+        dwl.add(dwf3);
+
+
+        for (Dwarf dwf:dwl ) {
+            System.out.println(dwf.getName());
+        }
+
+        Collections.sort(dwl);
+
+        for (Dwarf dwf:dwl ) {
+            System.out.println(dwf.getName());
+        }
+
+    }
+}
